c1a76931747655ac3fc57a34cd395add129a7d4a
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-06-14 13:59:16 +0200

    Ajout Guard LoggedIn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DetailComponent } from './detail/detail.component';
 import { PATH_HOME, PATH_DETAIL, PATH_DETAIL_FORMATIONS, PATH_DETAIL_SKILLS } from './constantes.routes';
 import { FormationsComponent } from './formations/formations.component';
 import { ConnaissancesComponent } from './connaissances/connaissances.component';
+import { LoggedInGuard } from './Guards/logged-in.guard';
 
 export const ROUTES: Routes = [
     { path: PATH_HOME, component: HomeComponent },
@@ -12,8 +13,8 @@ export const ROUTES: Routes = [
         component: DetailComponent,
         children: [
             { path: '', pathMatch: 'full', redirectTo: PATH_DETAIL_FORMATIONS },
-            { path: PATH_DETAIL_FORMATIONS, component: FormationsComponent },
-            { path: PATH_DETAIL_SKILLS, component: ConnaissancesComponent }
+            { path: PATH_DETAIL_FORMATIONS, component: FormationsComponent, canActivate: [LoggedInGuard] },
+            { path: PATH_DETAIL_SKILLS, component: ConnaissancesComponent, canActivate: [LoggedInGuard] }
         ]
     }
 ];
